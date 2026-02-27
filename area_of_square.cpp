902e1aeca654a908c1c2d641ce281d7a579e6d63
SHAURYA THAKUR <shauryathakur1981@gmail.com>
2026-02-27 06:45:39 +0530

    Create area_of_square.cpp

---
+++ area_of_square.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+    float n ,area;
+    cout<<"Enter the length of the side of the square : ";
+    cin>>n;
+    area =n*n;
+    cout<<"The area of square is : "<<area;
+    return 0;
+}
