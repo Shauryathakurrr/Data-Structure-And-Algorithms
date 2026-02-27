0ea51563369360a84df217aaa1ffff07fc5c5ff5
SHAURYA THAKUR <shauryathakur1981@gmail.com>
2026-02-27 06:51:26 +0530

    Create minimum_of_two_numbers.cpp

---
+++ minimum_of_two_numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float a,b;
+cout<<"Enter the first number : ";
+cin>>a;
+cout<<"Enter the second number : ";
+cin>>b;
+if(a<b){
+    cout<<"The minimum no. is : "<<a;
+}
+else 
+{
+    cout<<"The minimum no. is : "<<b;
+}
+return 0;
+}
+
+    
