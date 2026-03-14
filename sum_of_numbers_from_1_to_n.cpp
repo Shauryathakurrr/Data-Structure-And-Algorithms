027392bfbcb83d6594f3469af5219eb88984cafa
SHAURYA THAKUR <shauryathakur1981@gmail.com>
2026-03-14 22:16:12 +0530

    Create sum_of_numbers_from_1_to_n.cpp
    sum from 1 to n

---
+++ sum_of_numbers_from_1_to_n.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n ,sum=0;
+cout<<"Enter a number : "<<endl;
+cin>>n;
+for (int i = 1; i<=n; i++)
+{
+    sum+=i;
+}
+cout<<"the sum of numbers are : "<<sum<<endl;
+return 0;
+
+}
