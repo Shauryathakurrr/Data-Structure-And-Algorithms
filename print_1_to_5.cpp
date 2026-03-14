3f69e456b79d0dd050909cfdae8b6d781b314bb1
SHAURYA THAKUR <shauryathakur1981@gmail.com>
2026-03-14 22:11:56 +0530

    Create print_1_to_5.cpp
    loops

---
+++ print_1_to_5.cpp
@@ -0,0 +1,27 @@
+// using for loop
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){ 
+//     for(int i=1; i<=5; i++){
+//         cout<<i<<" "<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+//using while loop
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){ 
+//     int i=1;
+//     while(i<=5){ 
+//         cout<<i<<" "<<endl;
+//         i++;
+//     }
+//     return 0;
+// }
